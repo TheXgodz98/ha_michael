7b9d88a9142a597e95d51dae1ed1dcfe1c17256b
Niccolò Dassie <niccolo.dassie@gmail.com>
2026-06-24 23:14:05 +0200

    Collega i sensori TVOC alle zone clima corrispondenti
    Zona Giorno -> sensor.tvoc_zona_1, Bagno -> sensor.tvoc_zona_2,
    Camera -> sensor.tvoc_zona_3. Cameretta/Cabina Armadio/Lavanderia
    non hanno sensore VOC installato. Bump versione add-on a 0.7.1.

--- addons/dashboard_custom/app/src/zones.js
+++ addons/dashboard_custom/app/src/zones.js
@@ -1,8 +1,8 @@
 export const ZONES = [
-  { entity: "climate.zona_giorno", name: "Zona Giorno", icon: "sofa", voc: null },
-  { entity: "climate.bagno", name: "Bagno", icon: "bath", voc: null },
+  { entity: "climate.zona_giorno", name: "Zona Giorno", icon: "sofa", voc: "sensor.tvoc_zona_1" },
+  { entity: "climate.bagno", name: "Bagno", icon: "bath", voc: "sensor.tvoc_zona_2" },
   { entity: "climate.cameretta", name: "Cameretta", icon: "bed", voc: null },
-  { entity: "climate.camera", name: "Camera", icon: "moon", voc: null },
+  { entity: "climate.camera", name: "Camera", icon: "moon", voc: "sensor.tvoc_zona_3" },
   { entity: "climate.cabina_armadio", name: "Cabina Armadio", icon: "wardrobe", voc: null },
   { entity: "climate.lavanderia", name: "Lavanderia", icon: "laundry", voc: null },
 ];
